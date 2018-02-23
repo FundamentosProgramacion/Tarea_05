595f5c6b99c7c9cb7d3e8307fc626078ad3724d9
Jossian <A01746745@itesm.mx>
2018-02-23 10:16:36 -0600

    comienzo dibujos

--- dibujarCiclos.py
+++ dibujarCiclos.py
@@ -9,9 +9,10 @@ import pygame
 def dibujar():
     Ancho=800
     Alto=800
-
+    Azul= (33, 81, 238)
+    BLANCO=(255, 255, 255)
     pygame.init()
-    ventana = pygame.display.set_mode((ANCHO, ALTO))
+    ventana = pygame.display.set_mode((Ancho, Alto))
     reloj = pygame.time.Clock()
     termina = False
     while not termina:
@@ -19,12 +20,15 @@ def dibujar():
             if evento.type == pygame.QUIT:
                 termina = True
 
-         ventana.fill(BLANCO)
-        for cuadrado in range(10,800,10):
-            pygame.draw.circle(pass)
+        ventana.fill(BLANCO)
+        #for x in range(,10):
+
+        pygame.draw.circle(ventana, Azul, (Ancho//2, Alto//2), Ancho//2, 2)
+        pygame.draw.rect(ventana, Azul, (10, 10, Ancho-10, Alto-10), 2)
 
         pygame.display.flip()
         reloj.tick(40)
+
     pygame.quit()
 def main():
     dibujar()
